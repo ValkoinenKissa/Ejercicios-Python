b6bdae534512b25b41baa754e2731051d679f513
Alberto Bollo <albertobollo7@protonmail.com>
2024-02-09 13:37:09 +0100

    Actualización nuevos ejercicios

--- Algoritmos_ordenacion/Ordenacion_listas.py
+++ Algoritmos_ordenacion/Ordenacion_listas.py
@@ -6,8 +6,9 @@ class Lista:
         stop = False
         while not stop:
             dato = input("Introduce los datos que quieras cargar en la lista: ")
-            salir = (dato == "fin")
-            if not salir:
+            if dato == "fin":
+                stop = True
+            elif not stop:
                 self.lista.append(dato)
         return self.lista
 
@@ -15,48 +16,67 @@ class Lista:
         for elemento in self.lista:
             print(elemento)
 
+    def consulta_secuencial(self):
+        buscado = input("Introduce los datos que quieres buscar: ")
+        posicion = self.busqueda_secuencial(buscado)
+        existe = posicion != -1
+
+        if existe:
+            print("Encontrado ", buscado, " en la posicion ", posicion)
+
+        else:
+            print("No se ha encontrado el elemento en la lista")
+
+    def busqueda_secuencial(self, elemento):
+        encontrado = False
+
+        for item in self.lista:
+            if item == elemento:
+                encontrado = True
+
+        return encontrado
+
     def menu_programa(self):
-        opcion = 0
+        print("--------------------------------")
+        print("Menu listas")
+        print("1- Cargar lista")
+        print("2- Imprimir")
+        print("3- Busqueda secuencial")
+        print("4- Ordenar lista")
+        print("5- Consulta binaria")
+        print("6- Salir")
+        opcion = int(input("Introduce la opcion elegida...."))
+
         stop = False
         while not stop:
-            print("--------------------------------")
-            print("Menu listas")
-            print("1- Cargar lista")
-            print("2- Imprimir")
-            print("3- Busqueda secuencial")
-            print("4- Ordenar lista")
-            print("5- Consulta binaria")
-            print("6- Salir")
-            opcion = int(input("Introduce la opcion elegida...."))
 
-        match opcion:
-            case 1:
-                self.cargar_lista()
+            match opcion:
+                case 1:
+                    self.cargar_lista()
 
-            case 2:
-                self.imprimir_lista()
+                case 2:
+                    self.imprimir_lista()
 
-            case 3:
-                pass
+                case 3:
+                    self.consulta_secuencial()
 
-            case 4:
-                pass
+                case 4:
+                    pass
 
-            case 5:
-                pass
+                case 5:
+                    pass
 
-            case 6:
-                stop = True
+                case 6:
+                    stop = True
 
-            case other:
-                print("La opcion introducida no es correcta...")
+                case other:
+                    print("La opcion introducida no es correcta...")
 
 
 def main():
-    lista1 = []
-    lista = Lista(lista1)
-    lista.cargar_lista()
-    lista.imprimir_lista()
+    lista_1 = []
+    lista = Lista(lista_1)
+    lista.menu_programa()
 
 
 main()
