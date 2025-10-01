b2d9f50b5f13ebd9b0a1ceec31811972f24dfa8e
Alberto Bollo <albertobollo7@protonmail.com>
2025-10-01 23:13:20 +0200

    Fixed exercise

--- Arrays_y_listas/Ejercicio1.py
+++ Arrays_y_listas/Ejercicio1.py
@@ -7,13 +7,16 @@ obtener la longitud del lista
 
 """
 lista_vacia = []
+contador = 5
 
-for i in range(0, 11):
-    lista_vacia.append(input("Escribe lo que quieras"))
+for i in range(0, 5):
+    lista_vacia.append(input("Escribe lo que quieras: "))
+    print("Registro actualizado, espacio restante: ", contador, " espacios")
+    contador -= 1
 
-lista_con_datos = lista_vacia
+longitud_lista = lista_vacia
 print("Aqui se imprime la lista")
-print(lista_con_datos)
+print(longitud_lista)
 
 print("Aqui se imprime la longitud de la lista: ")
-print(len(lista_con_datos))
+print(len(longitud_lista))
